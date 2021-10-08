c40a0a52d00580a9abeecca58ae95f733ec7d534
RashiKhannamb2 <92031738+RashiKhannamb2@users.noreply.github.com>
2021-10-09 00:40:14 +0300

    Update count.py

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -14,7 +14,7 @@ def countNode(update, context):
     args = update.message.text.split(" ", maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        msg = sendMessage(f"Counting: <code>{link}</code>", context.bot, update)
+        msg = sendMessage(f"<b>🤒 Counting... :</b> <code>{link}</code>", context.bot, update)
         gd = GoogleDriveHelper()
         result = gd.count(link)
         deleteMessage(context.bot, msg)
@@ -23,10 +23,10 @@ def countNode(update, context):
         else:
             uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
         if uname is not None:
-            cc = f'\n\n<b>cc: </b>{uname}'
+            cc = f'\n\n<b>👤 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐁𝐲 : {uname}</b>'
         sendMessage(result + cc, context.bot, update)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Count.", context.bot, update)
+        sendMessage("<b>📎 Provide G-Drive Shareable Link to Count 🤒</b>", context.bot, update)
 
 count_handler = CommandHandler(BotCommands.CountCommand, countNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(count_handler)
